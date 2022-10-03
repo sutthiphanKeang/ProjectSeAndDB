c0b16d31084ab93a81351e1a0d86e6462fb61d2a
sutthiphanKeang <Sutthiphan_pranan@cmu.ac.th>
2022-10-04 00:27:53 +0700

    semi final not send to backend yet

--- rent_car_project/src/components/admin/AddCar.tsx
+++ rent_car_project/src/components/admin/AddCar.tsx
@@ -9,6 +9,7 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import SendIcon from "@mui/icons-material/Send";
+import ReactDOM from "react-dom/client";
 
 const theme = createTheme({
   palette: {
@@ -28,151 +29,211 @@ const theme = createTheme({
   },
 });
 
-export default class AddCar extends Component {
-  render() {
-    return (
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={12}
-      >
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="225"
-            image="/static/images/cards/contemplative-reptile.jpg"
-          />
-          <CardContent>
-            <ThemeProvider theme={theme}>
+const AddCar: React.FC = () => {
+  const [carName, setcarName] = useState("");
+  const [carId, setcarId] = useState("");
+  const [description, setdescription] = useState("");
+  const [review, setreview] = useState("");
+  const [price, setprice] = useState("");
+  const [file, setfile] = useState<FileList | null>();
+
+  const handleSubmit = () => {
+    console.log(`handleSubmit`);
+    const body = new FormData()
+    body.append('carName',carName)
+    body.append('carId',carId)
+    body.append('description',description)
+    body.append('review',review)
+    body.append('price',price)
+    body.append('file', file ? file[0] : "./Screen Shot 2565-10-03 at 23.02.04.png")
+    
+    fetch("http://localhost:5500/vehicle/", {
+      mode: 'no-cors',
+      body: body,
+      method: "POST",
+      headers: {
+        "Content-type": "multipart/form-data",
+        "Accept": "application/json",
+        "type": "formData"
+      },
+      // body: JSON.stringify({
+      //   carName: carName,
+      //   carId: carId,
+      //   description: description,
+      //   review: review,
+      //   price: price,
+      //   file: file ? file[0] : "./Screen Shot 2565-10-03 at 23.02.04.png",
+      // }),
+      
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+
+    // fetch("http://localhost:5500/vehicle/", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "image/png",
+    //   },
+    //   body: file ? file[0] : "./Screen Shot 2565-10-03 at 23.02.04.png",
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data))
+    //   .then((success) => console.log(success))
+    //   .catch((err) => console.log(err));
+  };
+  console.log("carName 👉️", carName);
+  console.log("carId 👉️", carId);
+  console.log("description 👉️", description);
+  console.log("review 👉️", review);
+  console.log("price 👉️", price);
+  console.log("file 👉️", file);
+
+  // setcarName("")
+  // setcarId("")
+  // setdescription("")
+  // setreview("")
+  // setprice("")
+  // setfile(null)
+
+  return (
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={12}
+    >
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="225"
+          image={
+            file
+              ? URL.createObjectURL(file[0])
+              : "/static/images/cards/contemplative-reptile.jpg"
+          }
+        />
+        <CardContent>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                boxShadow: 1,
+                borderRadius: 2,
+                p: 2,
+                minWidth: 435,
+              }}
+            >
               <Box
                 sx={{
-                  bgcolor: "background.paper",
-                  boxShadow: 1,
-                  borderRadius: 2,
-                  p: 2,
-                  minWidth: 435,
+                  color: "text.primary",
+                  fontSize: 34,
+                  fontWeight: "medium",
                 }}
               >
-                <Box
-                  sx={{
-                    color: "text.primary",
-                    fontSize: 34,
-                    fontWeight: "medium",
-                  }}
-                >
-                  ชื่อรถ
-                </Box>
-                <Box
-                  sx={{
-                    color: "text.secondary",
-                    fontSize: 25,
-                    fontWeight: "medium",
-                  }}
-                >
-                  รายละเอียด
-                </Box>
-                <Box
-                  sx={{
-                    color: "text.secondary",
-                    fontSize: 25,
-                    fontWeight: "medium",
-                  }}
-                >
-                  รีวิว
-                </Box>
-
-                <Box
-                  sx={{
-                    color: "success.dark",
-                    display: "inline",
-                    fontWeight: "bold",
-                    fontSize: 29,
-                  }}
-                >
-                  ราคา
-                </Box>
+                {carName}
               </Box>
-            </ThemeProvider>
-          </CardContent>
-        </Card>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardContent>
-            <h1>Import Data</h1>
-            <form method="POST" action="/">
-              <TextField
-                id="outlined-basic"
-                label="Car Name"
-                variant="outlined"
-                sx={{ m: 1 }}
+              <Box
+                sx={{
+                  color: "text.secondary",
+                  fontSize: 25,
+                  fontWeight: "medium",
+                }}
               >
-                <input type="text" placeholder="carName" name="carName"></input>
-              </TextField>
-              <TextField
-                id="outlined-basic"
-                label="Car ID"
-                variant="outlined"
-                sx={{ m: 1 }}
+                {carId}
+              </Box>
+              <Box
+                sx={{
+                  color: "text.secondary",
+                  fontSize: 25,
+                  fontWeight: "medium",
+                }}
               >
-                <input type="text" placeholder="carId" name="carId"></input>
-              </TextField>
-              <TextField
-                id="outlined-multiline-static"
-                label="Description"
-                multiline
-                rows={4}
-                sx={{ m: 1 }}
+                {description}
+              </Box>
+              <Box
+                sx={{
+                  color: "success.dark",
+                  display: "inline",
+                  fontWeight: "bold",
+                  fontSize: 29,
+                }}
               >
-                <input
-                  type="text"
-                  placeholder="description"
-                  name="description"
-                ></input>
-              </TextField>
-              <TextField
-                id="outlined-multiline-static"
-                label="Review"
-                multiline
-                rows={4}
-                sx={{ m: 1 }}
+                {review}
+              </Box>
+              <Box
+                sx={{
+                  color: "success.dark",
+                  fontWeight: "bold",
+                  fontSize: 29,
+                }}
               >
-                <input type="text" placeholder="review" name="review"></input>
-              </TextField>
-              <TextField
-                id="outlined-basic"
-                label="Price"
-                variant="outlined"
-                sx={{ m: 1 }}
-              >
-                <input type="text" placeholder="price" name="price"></input>
-              </TextField>
+                {price}
+              </Box>
+            </Box>
+          </ThemeProvider>
+        </CardContent>
+      </Card>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <h1>Import Data</h1>
+          <form encType='multipart/form-data'>
+            <input
+              type="text"
+              placeholder="carName"
+              name="carName"
+              onChange={(event) => setcarName(event.target.value)}
+              value={carName}
+            ></input>
+            <input
+              type="text"
+              placeholder="carId"
+              name="carId"
+              onChange={(event) => setcarId(event.target.value)}
+              value={carId}
+            ></input>
+            <input
+              type="text"
+              placeholder="description"
+              name="description"
+              onChange={(event) => setdescription(event.target.value)}
+              value={description}
+            ></input>
+            <input
+              type="text"
+              placeholder="review"
+              name="review"
+              onChange={(event) => setreview(event.target.value)}
+              value={review}
+            ></input>
+            <input
+              type="text"
+              placeholder="price"
+              name="price"
+              onChange={(event) => setprice(event.target.value)}
+              value={price}
+            ></input>
+            {/* <form method="POST" action="http://localhost:5500/vehicle/img" encType="multipart/form-data"> */}
+            <input
+              placeholder="file"
+              type="file"
+              name="file"
+              onChange={(event) => setfile(event.target.files)}
+            ></input>
 
-              <Stack direction="row" alignItems="center" spacing={2}>
-                <Button variant="contained" component="label" sx={{ m: 1 }}>
-                  Upload
-                  <input hidden multiple type="file" name="image" />
-                </Button>
-                <IconButton
-                  color="primary"
-                  aria-label="upload picture"
-                  component="label"
-                >
-                  <input hidden type="file" name="image" />
-                  <PhotoCamera />
-                </IconButton>
-              </Stack>
-              <Button
-                variant="contained"
-                endIcon={<SendIcon />}
-                type="submit"
-                sx={{ m: 1 }}
-              >
-                Send
-              </Button>
-            </form>
-          </CardContent>
-        </Card>
-      </Stack>
-    );
-  }
-}
+            <Stack direction="row" alignItems="center" spacing={2}></Stack>
+            <Button
+              variant="contained"
+              endIcon={<SendIcon />}
+              type="submit"
+              sx={{ m: 1 }}
+              onClick={handleSubmit}
+            >
+              Send
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </Stack>
+  );
+};
+export default AddCar;
