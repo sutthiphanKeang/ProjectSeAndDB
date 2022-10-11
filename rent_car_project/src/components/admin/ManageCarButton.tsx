942a7ea7a9ee24f9486e813b1b90bbdb7127ac43
Wachiranan Phuangpanya <wachiranan_p@cmu.ac.th>
2022-10-11 21:47:24 +0700

    add manage car
    can edit and send to backend

--- rent_car_project/src/components/admin/ManageCarButton.tsx
+++ rent_car_project/src/components/admin/ManageCarButton.tsx
@@ -1,4 +1,5 @@
 import {
+  Autocomplete,
   Box,
   Button,
   Dialog,
@@ -7,21 +8,22 @@ import {
   DialogContentText,
   DialogTitle,
   Grid,
-  IconButton,
   Slide,
   TextField,
-  Typography,
 } from "@mui/material";
 import React, { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { TransitionProps } from "@mui/material/transitions/transition";
 import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
+import { type } from "@testing-library/user-event/dist/type";
 
 type props = {
   title?: any;
   img?: any;
   id?: any;
+  year?: any;
+  brand?: any;
 };
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -33,18 +35,115 @@ const Transition = React.forwardRef(function Transition(
 });
 interface State {
   carName: string;
-  carID: any;
+  carID: string;
   description: string;
   review: string;
-  price: number;
+  price: string;
+  preview: any;
+  raw: any;
+  typeID: string;
 }
-const ManageCarButton: React.FC<props> = ({ title, img, id }) => {
+const ManageCarButton: React.FC<props> = ({ title, img, id ,year,brand}) => {
+  const options = ["Sedan", "Van", "Motorcycle", "Hatchback", "Coupe", "SUV", "PPV", "Pickup", "Sport", "Super"];
   const [open, setOpen] = useState(false);
+  const [inputValue, setInputValue] = React.useState("");
+  const [typeCar, settypeCar] = React.useState("");
+  const typeId = new Map<string, string>([
+    ["Sedan", "1"],
+    ["Van", "2"],
+    ["Motorcycle", "3"],
+    ["Hatchback", "4"],
+    ["Coupe", "5"],
+    ["SUV", "6"],
+    ["PPV", "7"],
+    ["Pickup", "8"],
+    ["Sport", "9"],
+    ["Super", "10"],
+  ]);
+
+  const [values, setValues] = React.useState<State>({
+    carName: brand+" "+ title+" "+year,
+    carID: "",
+    description: "",
+    review: "",
+    price: "",
+    preview: "",
+    raw: "",
+    typeID: "",
+  });
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+  const handleChangeImage = (e: any) => {
+    if (e.target.files.length) {
+      setValues({
+        carName: values.carName,
+        carID: values.carID,
+        description: values.description,
+        review: values.review,
+        price: values.price,
+        preview: URL.createObjectURL(e.target.files[0]),
+        raw: e.target.files[0],
+        typeID: values.typeID,
+      });
+    }
+  };
+  // const handleUpload = async e => {
+  //   e.preventDefault();
+  //   const formData = new FormData();
+  //   formData.append("image", image.raw);
+
+  //   await fetch("YOUR_URL", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "multipart/form-data"
+  //     },
+  //     body: formData
+  //   });
+  // };
   const handleOpen = () => {
     setOpen(true);
   };
+  const handleNext = (e: React.MouseEvent) => {
+    e.preventDefault();
+    var body = new FormData();
+    body.append("carName", values.carName);
+    body.append("carId", id);
+    body.append("description", values.description);
+    body.append("review", values.review);
+    body.append("price", values.price);
+    body.append("typeId", typeCar);
+    body.append("file", values.raw);
+
+    const token = JSON.parse(localStorage.getItem("user") ?? '{token:""}').token;
+    console.log("token", token);
+
+    fetch("http://localhost:5500/vehicle/edit", {
+      mode: "cors",
+      body: body,
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .then(handleClose);
+  };
   const handleClose = () => {
     setOpen(false);
+    setValues({
+      carName: "",
+      carID: "",
+      description: "",
+      review: "",
+      price: "",
+      preview: "",
+      raw: "",
+      typeID: "",
+    });
   };
   return (
     <>
@@ -69,25 +168,91 @@ const ManageCarButton: React.FC<props> = ({ title, img, id }) => {
           <DialogContentText id="alert-dialog-slide-description">
             <Grid
               container
-              spacing={2}
-              justifyContent="center"
+              // spacing={1}
+              // justifyContent="space-around"
               alignItems="center"
             >
-              <Grid item xs={6} sx={{ height: "100%" }}>
-                <IconButton
-                  sx={{
-                    width: "100%",
-                    borderRadius: "0.5em",
-                    border: "1px solid",
-                    borderColor: "primary.main",
-                  }}
-                  aria-label="CarImage"
-                >
-                  <InsertPhotoIcon
-                    sx={{ color: "primary.main", fontSize: 250 }}
+              <Grid
+                item
+                style={{ flexGrow: "1" }}
+                xs={6}
+                justifyContent="center"
+                alignItems="center"
+              >
+                {values.preview ? (
+                  <>
+                    <Grid>
+                      <img
+                        src={values.preview}
+                        alt="dummy"
+                        width="100%"
+                        height="100%"
+                      />
+                    </Grid>
+                    <Grid
+                      item
+                      style={{ flexGrow: "1" }}
+                      justifyContent="center"
+                      alignItems="center"
+                    >
+                      <Button
+                        variant="outlined"
+                        startIcon={
+                          <InsertPhotoIcon
+                            sx={{
+                              color: "primary.main",
+                              alignItems: "center",
+                              display: "flex",
+                            }}
+                          />
+                        }
+                        component="label"
+                      >
+                        UPLOAD
+                        <input
+                          accept="image/*"
+                          id="icon-button-file"
+                          type="file"
+                          style={{ display: "none" }}
+                          onChange={handleChangeImage}
+                        />
+                      </Button>
+                    </Grid>
+                  </>
+                ) : (
+                  <>
+                  <img
+                        src={img}
+                        alt="dummy"
+                        width="100%"
+                        height="100%"
                   />
-                </IconButton>
+                  <Button
+                    variant="outlined"
+                    startIcon={
+                      <InsertPhotoIcon
+                        sx={{
+                          color: "primary.main",
+                          alignItems: "center",
+                          display: "flex",
+                        }}
+                        />
+                      }
+                      component="label"
+                      >
+                    UPLOAD
+                    <input
+                      accept="image/*"
+                      id="icon-button-file"
+                      type="file"
+                      style={{ display: "none" }}
+                      onChange={handleChangeImage}
+                      />
+                  </Button>
+                      </>
+                )}
               </Grid>
+
               <Grid item xs={6} justifyContent="center" alignItems="center">
                 <Box
                   component="form"
@@ -97,19 +262,53 @@ const ManageCarButton: React.FC<props> = ({ title, img, id }) => {
                   noValidate
                   autoComplete="off"
                 >
-                  <TextField id="carName" label="carName" variant="outlined" />
-                  <TextField id="carId" label="carId" variant="outlined" />
+                  <TextField
+                    id="carName"
+                    label="carName"
+                    variant="standard"
+                    value={values.carName}
+                    onChange={handleChange("carName")}
+                  />
+                  
                   <TextField
                     id="description"
                     label="description"
-                    variant="outlined"
+                    variant="standard"
+                    value={values.description}
+                    onChange={handleChange("description")}
                   />
-                  <TextField id="review" label="review" variant="outlined" />
-                  <TextField id="price" label="price" variant="outlined" />
                   <TextField
-                    id="outlined-basic"
-                    label="Outlined"
-                    variant="outlined"
+                    id="review"
+                    label="review"
+                    variant="standard"
+                    value={values.review}
+                    onChange={handleChange("review")}
+                  />
+                  <TextField
+                    id="price"
+                    label="price"
+                    variant="standard"
+                    value={values.price}
+                    onChange={handleChange("price")}
+                  />
+                  <Autocomplete
+                    value={values.typeID}
+                    onChange={(event: any, newValue: string | null) => {
+                      if (newValue) {
+                        settypeCar(typeId.get(newValue)!);
+                      }
+                    }}
+                    inputValue={inputValue}
+                    onInputChange={(event, newInputValue) => {
+                      setInputValue(newInputValue);
+                    }}
+                    id="type-id-car"
+                    options={options}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Type Car" variant="standard" />
+                    )}
+                    disablePortal
                   />
                 </Box>
               </Grid>
@@ -117,7 +316,7 @@ const ManageCarButton: React.FC<props> = ({ title, img, id }) => {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button color="success" onClick={handleClose}>
+          <Button color="success" onClick={handleNext}>
             Confirm
           </Button>
           <Button color="error" onClick={handleClose}>
