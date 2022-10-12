8187f7da1cc3fbf7f281b5eb32669059f21cfd7a
spnaay <nxne2307@gmail.com>
2022-10-12 17:47:33 +0700

    fix firsthome

--- rent_car_project/src/components/FirstHome.tsx
+++ rent_car_project/src/components/FirstHome.tsx
@@ -1,37 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import Box from "@mui/material/Box";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 export default class FristHome extends Component {
+  
   render() {
     const slideImages = [
       {
-        url: "img/Car1.jpg",
-        caption: "Slide 1",
+        url: 'img/DOG.jpg',
       },
       {
-        url: "img/dog1.jpg",
-        caption: "Slide2",
-      },
-    ];
+        url: 'img/dog1.jpg',
+      }];
     return (
       <div className="slide-container">
-        <Box sx={{ mt: 3 }}>
-          <Slide>
-            {slideImages.map((slideImage, index) => (
-              <div className="each-slide" key={index}>
-                <div
-                  style={{
-                    backgroundImage: `url(${slideImage.url})`,
-                    height: 600,
-                  }}
-                ></div>
+        <Box sx={{mt:3}}>
+          <Slide >
+         {slideImages.map((slideImage, index)=> (
+            <div className="each-slide" key={index}>
+              <div style={{'backgroundImage': `url(${slideImage.url})`, height: 400}}>
               </div>
-            ))}
-          </Slide>
+            </div>
+          ))} 
+        </Slide>
         </Box>
+        
       </div>
-    );
+    )
   }
 }
