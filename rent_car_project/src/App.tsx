4f88fe10f0f4cf553464fbd9415e742472b95ca3
sutthiphanKeang <Sutthiphan_pranan@cmu.ac.th>
2022-10-14 17:24:08 +0700

    fix route in app.tsx

--- rent_car_project/src/App.tsx
+++ rent_car_project/src/App.tsx
@@ -9,19 +9,19 @@ import FirstHome from "./components/FirstHome";
 import MainLayoutUser from "./components/MainLayout-user";
 import MainLayoutAdmin from "./components/MainLayout-admin";
 import Manage from "./components/admin/ManageMain";
-import ManageCar from "./components/admin/ManageCar";
-import AddCar from "./components/admin/AddCar";
+// import ManageCar from "./components/admin/ManageCar";
+// import AddCar from "./components/admin/AddCar";
 import Login from "./components/Login-user";
 import LoginAdmin from "./components/Login-admin";
 import Register from "./components/Regis-user";
-import ReturnCar from './components/ReturnCar';
+// import ReturnCar from './components/ReturnCar';
 
-import Costsummary from './components/user/Costsummary';
-import Payment from './components/user/Payment';
-import Additional from './components/user/Additional';
+// import Costsummary from './components/user/Costsummary';
+// import Payment from './components/user/Payment';
+// import Additional from './components/user/Additional';
 
 
-import Insurance from './components/Insurance';
+// import Insurance from './components/Insurance';
 import UserPage from './components/UserPage';
 import RentCar from './components/RentCar';
 
@@ -31,11 +31,11 @@ function App() {
     <Routes>
       <Route path="/" element={<MainLayoutUser />}>
         <Route path="" element={<FirstHome />}/>
-        <Route path='ReturnCar' element={<ReturnCar/>}></Route>
+        {/* <Route path='ReturnCar' element={<ReturnCar/>}></Route>
         <Route path='Costsummary' element={<Costsummary/>}></Route>
         <Route path='Payment' element={<Payment/>}></Route>
         <Route path='Additional' element={<Additional/>}></Route>
-        <Route path='Insurance' element={<Insurance/>}></Route>
+        <Route path='Insurance' element={<Insurance/>}></Route> */}
         <Route path='UserPage' element={<UserPage/>}></Route>
         <Route path='RentCar' element={<RentCar/>}></Route>
         <Route path="Login" element={<Login />}></Route>
@@ -44,8 +44,8 @@ function App() {
       </Route>
       <Route path="/Admin" element={<MainLayoutAdmin />}>
         <Route path="Manage" element={<Manage />}></Route>
-        <Route path="ManageCar" element={<ManageCar />}></Route>
-        <Route path="AddCar" element={<AddCar />}></Route>
+        {/* <Route path="ManageCar" element={<ManageCar />}></Route>
+        <Route path="AddCar" element={<AddCar />}></Route> */}
         <Route path="Login" element={<LoginAdmin />}></Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
